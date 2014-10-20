063dd2bc71ee6e82112a9189c4915a8957d6ac26
Johanna <jweinsziehr@gmx.de>
2014-10-19 23:03:37 -0700

    add test_settings.py

--- bloggy/test_settings.py
+++ bloggy/test_settings.py
@@ -3,7 +3,7 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "test",
+        "NAME": "bloggy",
         "USER": os.environ.get('PG_USER'),
         "PASSWORD": os.environ.get('PG_PASSWORD'),
         "HOST": "127.0.0.1",
